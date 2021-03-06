fc1846c2a5945a26c00f44fcbc5ff298be07ec70
GDaniil17 <32881349+GDaniil17@users.noreply.github.com>
2021-03-06 17:04:38 +0300

    First commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,24 @@
-fun main(args: Array<String>)
-{
-    println("Привет Мир!)")
+fun main() {
+    val example = Queue<Int>()
+    example.enqueue(1)
+    example.enqueue(2)
+    example.enqueue(3)
+    example.enqueue(4)
+    example.enqueue(5)
+    println(example.dequeue())
+    println(example.dequeue())
+    example.enqueue(6)
+    example.enqueue(7)
+    println(example.lis)
+
+    val exampleTwo = Stack<Int>()
+    exampleTwo.push(1)
+    exampleTwo.push(2)
+    exampleTwo.push(3)
+    exampleTwo.push(4)
+    println(exampleTwo.pop())
+    println(exampleTwo.pop())
+    exampleTwo.push(5)
+    println(exampleTwo.lis)
+
 }
