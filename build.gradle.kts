a655d11e8820b1dab0d4d2b94006236e250f28c0
GDaniil17 <32881349+GDaniil17@users.noreply.github.com>
2021-03-18 13:28:21 +0300

    Test commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,18 +12,10 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test-junit"))
-}
-
 tasks.test {
     useJUnit()
 }
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClassName = "MainKt"
 }
