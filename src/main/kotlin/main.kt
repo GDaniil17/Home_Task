fa07d42ec18d96deeb0d55ff401245fe58a11a06
GDaniil17 <32881349+GDaniil17@users.noreply.github.com>
2021-03-31 22:33:50 +0300

    Done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,3 @@
 fun main() {
-    val seaAnimal = ObjectOfSeaAnimals()
-    val found = seaAnimal.getCertainAnimal("Turtle")
-    println(found)
-    val unitedClass = ConnectionClass(listOf<ListOfSeaAndEarthAnimals>())
-    println(unitedClass.connect())
-    println(unitedClass.sort())
-    println(unitedClass.groupThem())
-    println(unitedClass.accord())
+
 }
